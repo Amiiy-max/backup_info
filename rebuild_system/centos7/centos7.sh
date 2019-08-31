abeae693e502bccd2f33cee1f528677e17ced90f
gugegev5 <1725763838@qq.com>
2019-09-01 01:53:59 +0800

    Update centos7.sh

--- rebuild_system/centos7/centos7.sh
+++ rebuild_system/centos7/centos7.sh
@@ -44,4 +44,5 @@ cd /download/fonts
 cd /
 rm -rf /download/fonts
 echo y | sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 zsh
